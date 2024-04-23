d0a4d24fd075f8ed6f80ac5c6601aefaaae6f60f
manh <nguyenmanh1196@gmail.com>
2024-04-23 09:54:25 +0700

    fix block css cách nhau 1 dòng

--- routes/web.php
+++ routes/web.php
@@ -7,48 +7,16 @@ use App\Http\Controllers\Students\StudentDashboardController;
 use App\Http\Controllers\Login\ForgotController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-//Login
-route::prefix('login')->group(function (){
-    route::get('/',[LoginController::class,'loginAccount'])->name('login');
-    route::get('/forgot',[ForgotController::class,'fogortAccount'])->name('forgot');
+Route::prefix('login')->group(function (){
+    Route::get('/',[LoginController::class,'loginAccount'])->name('login');
+    Route::get('/forgot',[ForgotController::class,'fogortAccount'])->name('forgot');
 });
 
-
-//Admin
-route::prefix('admin')->group(function (){
-   //Admin Dashboard
-    route::get('/',[AdminDashboardController::class,'adminDashboard']);
-
+Route::prefix('admin')->group(function (){
+    Route::get('/',[AdminDashboardController::class,'adminDashboard']);
 });
 
-//Students
-route::prefix('students')->group(function (){
-   //Students Dashboard
-    route::get('/',[StudentDashboardController::class,'stDashboard']);
+Route::prefix('students')->group(function (){
+    Route::get('/',[StudentDashboardController::class,'stDashboard']);
 });
 
-
-
-//Route::group([
-//    'prefix' => 'qr-code',
-//    'as' => 'qr-code.',
-//], function () {
-//        Route::get('/', [QrCodeController::class, 'index'])
-//            ->name('index');
-//        Route::get('/download', [QrCodeController::class, 'download'])
-//            ->name('download')
-//            ->middleware('permission:admin.qr-code.list|admin.qr-code.download');
-//        Route::post('/store', [QrCodeController::class, 'store'])->name('store')->middleware('permission:admin.qr-code.store');
-//});
