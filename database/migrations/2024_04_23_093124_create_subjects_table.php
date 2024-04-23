58f9707151af5dac668580845d5a94923a303755
manh <nguyenmanh1196@gmail.com>
2024-04-23 21:38:36 +0700

    change data types, add created_at/updated_at into per table

--- database/migrations/2024_04_23_093124_create_subjects_table.php
+++ database/migrations/2024_04_23_093124_create_subjects_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',50);
+            $table->string('name',100);
             $table->integer('department_id');
+            $table->timestamp();
         });
     }
 
