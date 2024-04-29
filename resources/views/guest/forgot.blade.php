5118a89488e57ac12c2604ef2bafe2c7302a2fac
manh <nguyenmanh1196@gmail.com>
2024-04-29 23:31:32 +0700

    User Authentication

---
+++ resources/views/guest/forgot.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.guest.layout')
+@section('content')
+    <link rel="stylesheet" href="{{asset('css/login/forgot.css')}}">
+    <div class="container">
+        <div class="block">
+            <div class="title">
+                <h3 class="title_name"> Thay đổi mật khẩu </h3>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Email đăng nhập">
+                <label for="floatingInput">Email</label>
+            </div>
+            <div class="login_btn">
+                <button type="button" class="btn btn-warning">Gửi link</button>
+            </div>
+        </div>
+    </div>
+@endsection
